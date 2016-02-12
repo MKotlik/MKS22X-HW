a82100a1b15dd2e659738966bccf17f5a0594284
Mikhail Kotlik <mkotlik@stuy.edu>
2016-02-12 18:08:54 -0500

    Finished KnightBoard assignment. Need to speed up closed tour.

--- 03knightboard/DriverK.java
+++ 03knightboard/DriverK.java
@@ -1,8 +1,20 @@
 public class DriverK {
     public static void main(String[]args) {
-	KnightBoard mBoard = new KnightBoard(4);
-	System.out.println(mBoard.name());
-	System.out.println(mBoard.toString());
-	mBoard.printSolution();
+		KnightBoard mBoard;
+		if (args.length == 2) {
+			int col = Integer.parseInt(args[0]);
+			int row = Integer.parseInt(args[1]);
+			mBoard = new KnightBoard(col, row);
+		} else {
+			mBoard = new KnightBoard(4, 4);
+		}
+		System.out.println("Name: " + mBoard.name());
+		//System.out.println(mBoard.toString());
+		//mBoard.printSolution();
+		//System.out.println(mBoard);
+		System.out.println("Solved: " + mBoard.solve());
+		//System.out.println(mBoard);
+		System.out.println("Board: ");
+		mBoard.printSolution();
     }
 }
--- 03knightboard/KnightBoard.java
+++ 03knightboard/KnightBoard.java
@@ -1,36 +1,92 @@
 public class KnightBoard {
     private String name;
     private int[][] board;
-    private int size;
+    private int cols;
+	private int rows;
+	private int initCol;
+	private int initRow;
+	private static boolean closed = false;
+	private static boolean DEBUG = false;
     
-    public KnightBoard(int size) {
-	name = "KOTLIK,MIKHAIL";
-	this.size = size;
-	board = new int[size][size];
+    public KnightBoard(int cols, int rows) {
+		name = "KOTLIK,MIKHAIL";
+		this.cols = cols;
+		this.rows = rows;
+		board = new int[rows][cols];
     }
 
     public String name() {
-	return name;
+		return name;
     }
 
     public boolean solve() {
-	return true;
+		for (int row = 0; row < rows; row++) {
+			for (int col = 0; col < cols; col++) {
+				if (closed) {
+					initCol = col;
+					initRow = row;
+				}
+				if (solveH(row, col, 1)) return true;
+			}
+		}
+		return false;
     }
+	
+	public boolean solveH(int row, int col, int move) {
+		if (move > cols * rows) {
+			if (closed) {
+				return row == initRow && col == initCol;
+			} else {
+				return true;
+			}
+		}
+		if (! isOnBoard(row, col)) return false;
+		if (board[row][col] != 0) return false;
+		board[row][col] = move;
+		debug(toString());
+		if (solveH(row - 2, col + 1, move + 1)) {
+			return true;
+		} else if (solveH(row - 1, col + 2, move + 1)) {
+			return true;
+		} else if (solveH(row + 1, col + 2, move + 1)) {
+			return true;
+		} else if (solveH(row + 2, col + 1, move + 1)) {
+			return true;
+		} else if (solveH(row + 2, col - 1, move + 1)) {
+			return true;
+		} else if (solveH(row + 1, col - 2, move + 1)) {
+			return true;
+		} else if (solveH(row - 1, col - 2, move + 1)) {
+			return true;
+		} else if (solveH(row - 2, col - 1, move + 1)) {
+			return true;
+		}
+		board[row][col] = 0;
+		return false;
+	}
+	
+	private boolean isOnBoard(int row, int col) {
+		return row >= 0 && row < rows && col >= 0 && col < cols;
+	}
 
     public String toString() {
-	String boardStr = "";
-	for (int row = 0; row < size; row++) {
-	    String rowStr = "";
-	    for (int col = 0; col < size; col++) {
-		rowStr += board[row][col] + " ";
-	    }
-	    boardStr += rowStr + "\n";
-	}
-	return boardStr;
+		String boardStr = "";
+		for (int row = 0; row < rows; row++) {
+			String rowStr = "";
+			for (int col = 0; col < cols; col++) {
+				rowStr += board[row][col] + "\t";
+			}
+			boardStr += rowStr + "\n";
+		}
+		return boardStr;
     }
 
     public void printSolution() {
-	System.out.println("Eureka!");
+		System.out.println(toString());
     }
+	
+	private void debug(String output) {
+		if (DEBUG) System.out.println(output);
+	}
     
 }
