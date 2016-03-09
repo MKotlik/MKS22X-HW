ebe91feb4ac33b64836436bd2d0a2a2658339173
Mikhail Kotlik <mkotlik@stuy.edu>
2016-03-09 01:15:48 -0500

    Fixed & test partition for Quick Select

--- 07quickselect/Quick.java
+++ 07quickselect/Quick.java
@@ -1,4 +1,32 @@
+import java.util.Arrays;
 public class Quick {
+    public static void main(String[]args) {
+	int[] test1 = {};
+	System.out.println(Arrays.toString(test1));
+	partition(test1, 0, test1.length - 1);
+	System.out.println(Arrays.toString(test1));
+
+	int[] test2 = {-1};
+	System.out.println(Arrays.toString(test2));
+	partition(test2, 0, test2.length - 1);
+	System.out.println(Arrays.toString(test2));
+
+	int[] test3 = {2,-10};
+	System.out.println(Arrays.toString(test3));
+	partition(test3, 0, test3.length - 1);
+	System.out.println(Arrays.toString(test3));
+
+	int[] test4 = {10,-5,7,3,5,10};
+	System.out.println(Arrays.toString(test4));
+	partition(test4, 0, test4.length - 1);
+	System.out.println(Arrays.toString(test4));
+
+	int[] test5 = {10,-5,7,3,5,10,-1,-1,-2,-3,11,12,6,220,144,234,119,-1};
+	System.out.println(Arrays.toString(test5));
+	System.out.println("Partition point: " + partition(test5, 0, test5.length - 1));
+	System.out.println(Arrays.toString(test5));
+    }
+    
     public static int quickselect(int[] data, int k) {
 	return 0;
     }
@@ -7,14 +35,17 @@ public class Quick {
 	return 0;
     }
 
-    private static int partition(int[] data, int left, int right) {
-	int pivotInd = (int)(Math.random() * (data.length + 1));
-	int swapInd = left;
-	int swap = data[right];
-	data[right] = data[pivotInd];
+    private static int partition(int[] data, int leftInd, int rightInd) {
+	if (data.length <= 1) {
+	    return 0;
+	}
+	int pivotInd = (int)(Math.random() * (rightInd - leftInd)) + leftInd;
+	int swapInd = leftInd;
+	int swap = data[rightInd];
+	data[rightInd] = data[pivotInd];
 	data[pivotInd] = swap;
-	for (int i = left; i < right; i++) {
-	    if (data[i] < data[right]) {
+	for (int i = leftInd; i < rightInd; i++) {
+	    if (data[i] < data[rightInd]) {
 		swap = data[swapInd];
 		data[swapInd] = data[i];	
 		data[i] = swap;
@@ -22,10 +53,10 @@ public class Quick {
 	    }
 	}
 	swap = data[swapInd];
-	data[swapInd] = data[right];
-	data[right] = swap;
+	data[swapInd] = data[rightInd];
+	data[rightInd] = swap;
 	return swapInd;
-}
+    }
 	
     
 }
