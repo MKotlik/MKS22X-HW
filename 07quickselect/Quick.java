b10901231d83d5cda5d31964b88d291b40e22d67
Mikhail Kotlik <mkotlik@stuy.edu>
2016-03-09 23:49:42 -0500

    Finished quick sort

--- 07quickselect/Quick.java
+++ 07quickselect/Quick.java
@@ -1,7 +1,54 @@
 import java.util.Arrays;
+import java.util.Random;
 public class Quick {
     public static void main(String[]args) {
+	if (args.length > 0 && args[0].equals("large")) {
+	    Random randGen = new Random();
+	    int[] largeTest = new int[1000000]; //1 million element array
+	    int[] comparisonArr = new int[1];
+	    if (args.length == 2 && args[1].equalsIgnoreCase("verify")) {
+		comparisonArr = new int[largeTest.length];
+	    }
+	    for (int i = 0; i < largeTest.length; i++) {
+		largeTest[i] = randGen.nextInt(100001) - 50000;
+		//Ints from -50,000 to 50,000
+		if (args.length == 2 && args[1].equalsIgnoreCase("verify")) {
+		    comparisonArr[i] = largeTest[i];
+		}
+	    }
+	    quickSort(largeTest);
+	    if (args.length == 2 && args[1].equalsIgnoreCase("verify")) {
+		System.out.println("Sorted correctly: " + Arrays.equals(largeTest, comparisonArr));
+	    }
+	} else {
+	    //Sort Tests
+	    int[] test1 = {};
+	    System.out.println(Arrays.toString(test1));
+	    quickSort(test1);
+	    System.out.println(Arrays.toString(test1));
+	    
+	    int[] test2 = {-1};
+	    System.out.println(Arrays.toString(test2));
+	    quickSort(test2);
+	    System.out.println(Arrays.toString(test2));
+	    
+	    int[] test3 = {2,-10};
+	    System.out.println(Arrays.toString(test3));
+	    quickSort(test3);
+	    System.out.println(Arrays.toString(test3));
+	    
+	    int[] test4 = {10,-5,7,3,5,10};
+	    System.out.println(Arrays.toString(test4));
+	    quickSort(test4);
+	    System.out.println(Arrays.toString(test4));
+	    
+	    int[] test5 = {10,-5,7,3,5,10,-1,-1,-2,-3,11,12,6,220,144,234,119,-1};
+	    System.out.println(Arrays.toString(test5));
+	    quickSort(test5);
+	    System.out.println(Arrays.toString(test5));
+	}
 	//Select Tests
+	/*
 	int[] test1 = {};
 	System.out.println(Arrays.toString(test1));
 	System.out.println(quickselect(test1, 0));
@@ -21,6 +68,7 @@ public class Quick {
 	int[] test5 = {10,-5,7,3,5,10,-1,-1,-2,-3,11,12,6,220,144,234,119,-1};
 	System.out.println(Arrays.toString(test5));
 	System.out.println(quickselect(test5, 8));
+	*/
 	
 	//Partition Tests
 	/*
@@ -51,15 +99,15 @@ public class Quick {
 	*/
     }
 
-    public static int quickSort(int[]data) {
+    public static void quickSort(int[]data) {
 	quickSort(data, 0, data.length-1);
     }
 
     private static void quickSort(int[] data, int left, int right) {
 	if (left < right) {
 	    int pivot = partition(data, left, right);
-	    quickSort(data, left, k - 1);
-	    quickSort(data, k + 1, right);
+	    quickSort(data, left, pivot - 1);
+	    quickSort(data, pivot + 1, right);
 	}
     }
     
