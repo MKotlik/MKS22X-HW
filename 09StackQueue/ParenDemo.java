513df5608e1f57247fb6ec17f0622884dc067b54
Mikhail Kotlik <mkotlik@stuy.edu>
2016-03-29 18:51:29 -0400

    Continued working on ParenDemo.java, adding arraylist & length check

--- 09StackQueue/ParenDemo.java
+++ 09StackQueue/ParenDemo.java
@@ -1,9 +1,15 @@
+import java.util.*;
 public class ParenDemo {
-    public static void main(String[]args) {
-	if (args.length > 0) {
-	    System.out.println(isMatching(args[0]));
-	}
-
+    
+    public static void main(String[]args){
+	String input = "()()(([[]]))";
+	if(args.length > 0){
+	    input = args[0];
+	    System.out.println( isMatching(input)); 
+	}else{
+	    System.out.println("Usage:"); 
+	    System.out.println("java ParenDemo \"text\""); 
+	}	
     }
 
     public static boolean isMatching(String parenStr) {
@@ -12,8 +18,36 @@ public class ParenDemo {
 	    char cur = parenStr.charAt(i);
 	    if (cur == '(' || cur == '{' || cur == '<' || cur == '[') {
 		openStack.push(cur);
+	    }
+	}
+	ArrayList<String> openParens = new ArrayList<String>(Arrays.asList
+
+    public static boolean isMatching(String parenStr) {
+	MyStack<Character> openStack= new MyStack<Character>();
+	for (int i = 0; i < parenStr.length(); i++) {
+	    char cur = parenStr.charAt(i);
+	    if (cur == '(' || cur == '{' || cur == '<' || cur == '[') {
+		openStack.push(cur);
 	    } else if (cur == ')') {
-		if (openStack.peek == cur) {
+		if (openStack.peek() == '(') {
+		    openStack.pop();
+		} else {
+		    return false;
+		}
+	    } else if (cur == '}') {
+		if (openStack.peek() == '{') {
+		    openStack.pop();
+		} else {
+		    return false;
+		}
+	    } else if (cur == '>') {
+		if (openStack.peek() == '<') {
+		    openStack.pop();
+		} else {
+		    return false;
+		}
+	    } else if (cur == ']') {
+		if (openStack.peek() == '[') {
 		    openStack.pop();
 		} else {
 		    return false;
