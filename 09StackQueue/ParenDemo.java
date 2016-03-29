718c7b20d5e5fa6c8ae89b5ad9f871b39f7f2638
Mikhail Kotlik <mkotlik@stuy.edu>
2016-03-29 19:14:40 -0400

    Finished ParenDemo.java

--- 09StackQueue/ParenDemo.java
+++ 09StackQueue/ParenDemo.java
@@ -13,15 +13,27 @@ public class ParenDemo {
     }
 
     public static boolean isMatching(String parenStr) {
-	MyStack<Character> openStack= new MyStack<Character>();
+	MyStack<String> openStack= new MyStack<String>();
+	String[] openParensArray = {"(","{","<","["};
+	String[] closeParensArray = {")","}",">","]"};
+	ArrayList<String> openParens = new ArrayList<String>(Arrays.asList(openParensArray));
+	ArrayList<String> closeParens = new ArrayList<String>(Arrays.asList(closeParensArray));
 	for (int i = 0; i < parenStr.length(); i++) {
-	    char cur = parenStr.charAt(i);
-	    if (cur == '(' || cur == '{' || cur == '<' || cur == '[') {
+	    String cur = parenStr.substring(i,i+1);
+	    if (openParens.contains(cur)) {
 		openStack.push(cur);
+	    } else if (closeParens.contains(cur)) {
+		if (!openStack.isEmpty() && openStack.peek().equals(openParens.get(closeParens.indexOf(cur)))) {
+		    openStack.pop();
+		} else {
+		    return false;
+		}
 	    }
 	}
-	ArrayList<String> openParens = new ArrayList<String>(Arrays.asList
+	return openStack.isEmpty();
+    }
 
+    /*
     public static boolean isMatching(String parenStr) {
 	MyStack<Character> openStack= new MyStack<Character>();
 	for (int i = 0; i < parenStr.length(); i++) {
@@ -56,4 +68,5 @@ public class ParenDemo {
 	}
 	return true;
     }
+    */
 }
