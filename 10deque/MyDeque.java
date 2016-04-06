339fb9871d749ff80710a35494a9a0d967676ed9
Mikhail Kotlik <mkotlik@stuy.edu>
2016-04-06 00:48:14 -0400

    Finished MyDeque

--- 10deque/MyDeque.java
+++ 10deque/MyDeque.java
@@ -19,21 +19,42 @@ public class MyDeque<T> {
 
     //suppress this ONE function from
     //having warnings.
-    //@SuppressWarnings("unchecked")
+    @SuppressWarnings("unchecked")
     private void grow() {
 	T[] newData = (T[]) new Object[data.length * 2];
-	
+	int i;
+	int limit;
+	int newInd = 0;
+	if (end < start) {
+	    limit = data.length;
+	} else {
+	    limit = end;
+	}
+	for (i = start; i < limit; i++) {
+	    newData[newInd] = data[i];
+	    if (end < start && i == data.length-1) {
+		i = 0;
+		limit = end + 1;
+	    }
+	    newInd++;
+	}
+	data = newData;
     }
 
     public T getFirst() throws NoSuchElementException{
 	if (size == 0) {
 	    throw new NoSuchElementException();
+	} else {
+	    return data[start];
 	}
+	
     }
 
     public T getLast() throws NoSuchElementException{
 	if (size == 0) {
 	    throw new NoSuchElementException();
+	} else {
+	    return data[end];
 	}
     }
     
@@ -41,20 +62,50 @@ public class MyDeque<T> {
 	if (size == 0) {
 	    throw new NoSuchElementException();
 	}
+	T oldValue = data[start];
+	if (start == data.length - 1) {
+	    start = 0;
+	} else {
+	    start++;
+	}
+	return oldValue;
     }
 
     public T removeLast() throws NoSuchElementException{
 	if (size == 0) {
 	    throw new NoSuchElementException();
 	}
+	T oldValue = data[end];
+	if (end == 0) {
+	    end = data.length - 1;
+	} else {
+	    end--;
+	}
+	return oldValue;
     }
 
     
     public void addFirst(T value) {
-	
+	if (size == data.length) {
+	    grow();
+	}
+	if (start == 0) {
+	    start = data.length - 1;
+	} else {
+	    start--;
+	}
+	data[start] = value;
     }
 
     public void addLast(T value) {
-
+	if (size == data.length) {
+	    grow();
+	}
+	if (end == data.length - 1) {
+	    end = 0;
+	} else {
+	    end++;
+	}
+	data[end] = value;
     }
 }
