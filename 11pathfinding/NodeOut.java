e583d15526e2df59fa120567b1d30e89b1cec322
Mikhail Kotlik <mkotlik@stuy.edu>
2016-04-11 20:36:22 -0400

    Finished returnSolution in BetterMaze

--- 11pathfinding/NodeOut.java
+++ 11pathfinding/NodeOut.java
@@ -1,5 +1,6 @@
-public class Node {
+public class NodeOut {
     //Instance variables
+    private int size;
     private int row;
     private int col;
     private Node prev;
@@ -8,14 +9,20 @@ public class Node {
     public Node(int row, int col, Node prev) {
 	this(row,col);
 	this.prev = prev;
+	size = prev.getSize() + 1;
     }
 
     public Node(int row, int col) {
 	this.row = row;
 	this.col = col;
+	size = 1;
     }
 
     //Accessors
+
+    public int getSize() {
+	return size;
+    }
     
     public int getRow() {
 	return row;
