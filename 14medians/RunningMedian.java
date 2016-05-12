fa61df01bde6b028f92164aa4dc5522784719f72
Mikhail Kotlik <mkotlik@stuy.edu>
2016-05-11 21:43:04 -0400

    a few more logistical changes

--- 14medians/RunningMedian.java
+++ 14medians/RunningMedian.java
@@ -1,6 +1,9 @@
-public class Medians {
+import java.util.*;
+@SupressWarnings("unchecked")
+public class RunningMedian<T extends Comparable<T>> {
     //Instance variables
-    MyHeap
+    MyHeap<T> smallerVals;
+    MyHeap<T> biggerVals;
 
     //Constructors
 
